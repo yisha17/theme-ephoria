a3a0672c5b1acf838bfd84cdd90c7ffe8dbfaaca
kalusha92 <it.kaleb.fekadu@gmail.com>
2022-01-16 15:41:27 +0300

    Header Updated

--- header.php
+++ header.php
@@ -3,7 +3,6 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <!-- <title>Bootstrap Blog - B4 Template by Bootstrap Temple</title> -->
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="all,follow">
@@ -13,7 +12,8 @@
     ?>
   
   </head>
-  <body>
+  <body <?php body_class( 'yishak' ); ?>>
+   <?php wp_body_open(); ?>
     <header class="header">
       <!-- Main Navbar-->
       <nav class="navbar navbar-expand-lg">
@@ -33,92 +33,3 @@
           </div>
         </div>
         <div class="container">
-          <!-- Navbar Brand -->
-          <div class="navbar-header d-flex align-items-center justify-content-between">
-            <?php
-             if(function_exists('the_custom_logo')){
-                $custom_logo_id = get_theme_mod('custom_logo');
-                $logo = wp_get_attachment_image_src($custom_logo_id);
-             }
-            ?>
-            <img style="height:40px;"class="mb-2 mx-auto logo" src="<?php echo $logo[0] ?>" alt="logo" >
-            <!-- Navbar Brand --><a href="index.html" class="navbar-brand">
-              <?php echo get_bloginfo('name');?> 
-            </a>
-            <!-- Toggle Button-->
-            <button type="button" data-toggle="collapse" data-target="#navbarcollapse" aria-controls="navbarcollapse" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler"><span></span><span></span><span></span></button>
-          </div>
-          <!-- Navbar Menu -->
-          <div id="navbarcollapse" class="collapse navbar-collapse">
-
-            <?php  
-
-              wp_nav_menu(
-
-                array(
-                  'menu' => 'primary',
-                  'container' => '',
-                  'theme_location' => 'primary',
-                  'items_wrap' => '<ul id="" class="navbar-nav ml-auto">%3$s</ul>',
-                  'add_li_class'  => 'nav-item',
-                  'add_a_class'  => 'nav-link',
-                )
-              );
-
-            ?>
-
-            <!-- <ul class="navbar-nav ml-auto">
-              <li class="nav-item"><a href="index.html" class="nav-link active ">Home</a>
-              </li>
-              <li class="nav-item"><a href="blog.html" class="nav-link ">Blog</a>
-              </li>
-              <li class="nav-item"><a href="post.html" class="nav-link ">Post</a>
-              </li>
-              <li class="nav-item"><a href="#" class="nav-link ">Contact</a>
-              </li>
-            </ul> -->
-            <div class="navbar-text"><a href="#" class="search-btn"><i class="icon-search-1"></i></a></div>
-            <ul class="langs navbar-text"><a href="#" class="active">EN</a><span>           </span><a href="#">ES</a></ul>
-          </div>
-        </div>
-      </nav>
-    </header>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <!-- Bootstrap CSS -->
-
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-    <title><?php bloginfo('name')?></title>
-    <?php wp_head() ?>
-</head>
-<body <?php body_class()?>>
-
-<div class="heading">
-
-<header class="site-header">
-<h1>   <a href="<?php echo home_url()?>"><?php bloginfo('name')?></a>  </h1>
-<h4><?php bloginfo('description')?></h4>
-</header>
-</div>
-<nav class="navbar navbar-expand-md navbar-dark bg-dark" role="navigation">
-  <div class="container">   
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'your-theme-slug' ); ?>">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-        <?php
-        wp_nav_menu( array(
-            'theme_location'    => 'top-menu',
-            'depth'             => 4,
-            'container'         => 'div',
-            'container_class'   => 'collapse navbar-collapse',
-            'container_id'      => 'bs-example-navbar-collapse-1',
-            'menu_class'        => 'nav navbar-nav',
-            'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-            'walker'            => new WP_Bootstrap_Navwalker(),
-        ) );
-        ?>
-    </div>
-</nav>
