07f59a4eede8ffa2e0bf4935f2481e57c68aab10
dathrob <dev.misganaw.meseret@gmail.com>
2022-01-16 15:25:15 +0300

    structure seach

---
+++ search.php
@@ -0,0 +1,22 @@
+<?php
+    get_header();
+    ?>
+
+    
+    <div class="container">
+      <div class="row">
+        <!-- Latest Posts -->
+        <main class="posts-listing col-lg-8"> 
+          <div class="container">
+            <div class="row">
+
+</div>
+        </div>
+              </main>
+      </div>
+      </div>
+    
+
+    <?php
+    get_footer();
+    ?>
