16b35ee942c5102bb705fee8cda5d9ddb442113e
saron-solomon <61591182+saron-solomon@users.noreply.github.com>
2022-01-16 16:00:35 +0300

    404 page started

---
+++ 404.php
@@ -0,0 +1,25 @@
+
+    <?php
+    get_header();
+    ?>
+
+    
+    <div class="container">
+      <div class="row">
+        <!-- Latest Posts -->
+        <main class="posts-listing col-lg-8"> 
+          <div class="container">
+            <div class="row">
+                <h1>Page Not Found</h1>
+                
+              <?php
+              
+
+              ?>
+          </div>
+
+          <div class="row">
+                
+                
+    
+          </div>
