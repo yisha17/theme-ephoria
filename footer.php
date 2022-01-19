1d7513d2f11cb6e96e9e9ea79a91bf010ff7f1ac
Avstel <it.nathan.demissie@gmail.com>
2022-01-19 21:51:30 +0300

    footer-edit-3

--- footer.php
+++ footer.php
@@ -26,7 +26,47 @@
             </div>
 
             
-          >
+          </div>
+          <div class="col-md-4">
+            <div class="latest-posts">
+              <!-- <a href="#">
+                <div class="post d-flex align-items-center">
+                  <div class="image"><img src="wp-content/themes/Ephoria/assets/img/small-thumbnail-1.jpg" alt="..." class="img-fluid"></div>
+                  <div class="title"><strong>Hotels for all budgets</strong><span class="date last-meta">October 26, 2016</span></div>
+                </div></a><a href="#">
+                <div class="post d-flex align-items-center">
+                  <div class="image"><img src="wp-content/themes/Ephoria/assets/img/small-thumbnail-2.jpg" alt="..." class="img-fluid"></div>
+                  <div class="title"><strong>Great street atrs in London</strong><span class="date last-meta">October 26, 2016</span></div>
+                </div></a><a href="#">
+                <div class="post d-flex align-items-center">
+                  <div class="image"><img src="wp-content/themes/Ephoria/assets/img/small-thumbnail-3.jpg" alt="..." class="img-fluid"></div>
+                  <div class="title"><strong>Best coffee shops in Sydney</strong><span class="date last-meta">October 26, 2016</span></div>
+                </div></a> -->
+              <?php dynamic_sidebar('footer_3') ?>
+                
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="copyrights">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6" style="display:inline">
+                <span>
+                <p>
+                <?php dynamic_sidebar('copy_right') ?>
+                
+                </p>
+                </span>
+              
+            </div>
+            <div class="col-md-6 text-right">
+              <?php dynamic_sidebar('author') ?>
+                <!-- Please do not remove the backlink to Bootstrap Temple unless you purchase an attribution-free license @ Bootstrap Temple or support us at http://bootstrapious.com/donate. It is part of the license conditions. Thanks for understanding :)                         -->
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </footer>
     
