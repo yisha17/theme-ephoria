f8a7a8c1da440820d2b7aa6ceb70c995888adf49
kalusha92 <it.kaleb.fekadu@gmail.com>
2022-01-19 23:54:32 +0300

    Index php updated

--- index.php
+++ index.php
@@ -5,5 +5,35 @@
     
     <div class="container">
       <div class="row">
-      
+        <!-- Latest Posts -->
+        <main class="posts-listing col-lg-8"> 
+          <div class="container">
+            <div class="row">
+              <?php 
+
+                if( have_posts()){
+
+                  while( have_posts()){
+
+                    the_post();
+                    
+                    get_template_part('template-parts/post/content-archive');
+                    
+                  }
+                }
+              
+              
+              ?>
+          </div>
+          <?php page_pagination(); ?> 
+        </div>
+              </main>
+              <?php get_template_part('sidebar') ?>
+      </div>
+      </div>
+    
+
+    <?php
+    get_footer();
+    ?>
     
