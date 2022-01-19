3e77c3e0827d030c5ec7194e2f7641e0f3921732
dathrob <dev.misganaw.meseret@gmail.com>
2022-01-19 09:32:21 +0300

    widget tag

--- sidebar.php
+++ sidebar.php
@@ -16,3 +16,26 @@
             <div class="item d-flex justify-content-between"><a href="#">Tips</a><span>17</span></div>
             <div class="item d-flex justify-content-between"><a href="#">Local</a><span>25</span></div> -->
       </div>
+      <div class="widget tags">
+        <header>
+          <h3 class="h6">Tags</h3>
+        </header>
+        <ul class="list-inline">
+
+          <?php
+          $tags = get_tags(array(
+            'hide_empty' => false
+            ));
+         
+            foreach ($tags as $tag) {
+              echo '<li class="list-inline-item"><a href="#" class="tag">'. $tag->name.'</a></li>';
+            }
+          
+          ?>
+          <!-- <li class="list-inline-item"><a href="#" class="tag">#Business</a></li>
+          <li class="list-inline-item"><a href="#" class="tag">#Technology</a></li>
+          <li class="list-inline-item"><a href="#" class="tag">#Fashion</a></li>
+          <li class="list-inline-item"><a href="#" class="tag">#Sports</a></li>
+          <li class="list-inline-item"><a href="#" class="tag">#Economy</a></li> -->
+        </ul>
+      </div>
