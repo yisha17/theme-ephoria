a814ea875c1d47e3756b9eda39fe341eb6f2ea75
dathrob <dev.misganaw.meseret@gmail.com>
2022-01-19 09:30:01 +0300

    header for side bar

---
+++ sidebar.php
@@ -0,0 +1,18 @@
+div class="widget categories">
+        <header>
+          <h3 class="h6">Categories</h3>
+        </header>
+        <?php
+        $categories = get_categories('exclude=1&title_li=');
+        foreach ($categories as $cat) {
+          echo "<div class='item d-flex justify-content-between'><a href=\"" . $cat->category_nicename . "\">" . $cat->cat_name . "</a>
+                      <span>" . $cat->category_count . "</span></div>";
+        }
+        ?>
+        <!-- 
+            <div class="item d-flex justify-content-between"><a href="#">Growth</a><span>12</span></div>
+            <div class="item d-flex justify-content-between"><a href="#">Local</a><span>25</span></div>
+            <div class="item d-flex justify-content-between"><a href="#">Sales</a><span>8</span></div>
+            <div class="item d-flex justify-content-between"><a href="#">Tips</a><span>17</span></div>
+            <div class="item d-flex justify-content-between"><a href="#">Local</a><span>25</span></div> -->
+      </div>
